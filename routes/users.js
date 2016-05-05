6d561c21c633649e6902a41b714aa8f265194663
iamso1 <bmhiamso1@gmail.com>
2016-05-06 01:01:38 +0800

    add new rules to route

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.route('/users')
-  .post(function (req,res) {
-    res.send('user post');
+  .post(function (req, res) {
+    var user_name=req.body.user_name===undefined?'stranger':req.body.user_name;    
+    res.send('Welcome '+user_name);
   })
-  .get(function(req,res){
+  .get(function (req, res) {
     res.send('user get');
   });
 
+router.route('/users/:user_name')
+  .post(function (req, res) {
+    res.send(
+      res.json('hello '+ req.params.user_name)
+    );
+  });
 
 module.exports = router;
