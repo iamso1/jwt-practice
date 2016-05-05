80b13aaf6ca5d6924e8f5cc53ee0e7e69685609a
iamso1 <bmhiamso1@gmail.com>
2016-05-06 00:11:29 +0800

    add login to routes and app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var login = require('./routes/login.js');
 var users = require('./routes/users');
 
 var app = express();
@@ -21,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/login', login);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
